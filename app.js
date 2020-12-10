a8c9c68d50f0529cdd2a72bfdb42a7c3a1209115
Nayeem-uddin <nayeemu02@gmail.com>
2020-12-11 00:23:59 +0600

    login_registration form

---
+++ app.js
@@ -0,0 +1,74 @@
+// require('dotenv').config();
+require('dotenv/config')
+const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
+const mongoose = require('mongoose');
+const passport = require('passport');
+const flash = require('connect-flash');
+const session = require('express-session');
+
+const app = express();
+
+// Passport Config
+require('./config/passport')(passport);
+
+// DB Config
+const db = require('./config/keys').mongoURI;
+//connet database by why its not working . I was going to be depressed. whats going on
+// mongoose
+//   .connect(
+//     db,
+//     { useNewUrlParser: true ,useUnifiedTopology: true}
+//   )
+//   .then(() => console.log('MongoDB Connected'))
+//   .catch(err => console.log(err));
+
+//Alhamdulillah . Thanks stackoverflow. 
+// Connect to MongoDB
+//after trying almost one hour I have made it ...yahhoooo ..
+//Thanks stackoverflow by the way
+mongoose.connect(process.env.DB_CONNECTION, {useNewUrlParser: true});
+    mongoose.connection.once('open', function(){
+      console.log('Conection has been made!');
+    }).on('error', function(error){
+        console.log('Error is: ', error);
+});
+
+// EJS
+app.use(expressLayouts);
+app.set('view engine', 'ejs');
+
+// Express body parser
+app.use(express.urlencoded({ extended: true }));
+
+// Express session
+app.use(
+  session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true
+  })
+);
+
+// Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Connect flash
+app.use(flash());
+
+// Global variables
+app.use(function(req, res, next) {
+  res.locals.success_msg = req.flash('success_msg');
+  res.locals.error_msg = req.flash('error_msg');
+  res.locals.error = req.flash('error');
+  next();
+});
+
+// Routes
+app.use('/', require('./routes/index.js'));
+app.use('/users', require('./routes/users.js'));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, console.log(`Server running on  ${PORT}`));
---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    MongoURI:'mongodb+srv://nayeem:123@test-cluster.ewato.mongodb.net/<dbname>?retryWrites=true&w=majority'
+}
